08143554f6f12b3a4e49139d18b3216b11f2c277
Phumphuridon <66070162@kmitl.ac.th>
2024-10-13 01:40:56 +0700

    FULL DEMO

--- src/main/java/com/Boardzone/boardapi/BoardapiApplication.java
+++ src/main/java/com/Boardzone/boardapi/BoardapiApplication.java
@@ -10,5 +10,4 @@ public class BoardapiApplication {
                 
 		SpringApplication.run(BoardapiApplication.class, args);
 	}
-        
 }
--- src/main/java/com/Boardzone/boardapi/repository/UserRepository.java
+++ src/main/java/com/Boardzone/boardapi/repository/UserRepository.java
@@ -2,15 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Interface;
+package com.Boardzone.boardapi.repository;
 
-import Entity.UserEntity;
+import com.Boardzone.boardapi.entity.UserDemo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  *
  * @author phump
  */
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    
+public interface UserRepository extends JpaRepository<UserDemo, Integer>{
 }
