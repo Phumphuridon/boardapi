1b13151410b5a3efe2a644ed441e238a224bbe21
Phumphuridon <66070162@kmitl.ac.th>
2024-10-12 16:41:49 +0700

    Start

---
+++ src/main/java/com/Boardzone/boardapi/BoardapiApplication.java
@@ -0,0 +1,14 @@
+package com.Boardzone.boardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardapiApplication {
+
+	public static void main(String[] args) {
+                
+		SpringApplication.run(BoardapiApplication.class, args);
+	}
+        
+}
---
+++ src/test/java/com/Boardzone/boardapi/BoardapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Boardzone.boardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
