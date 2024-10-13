d54d9d6b120d1739e56b018876971805e0a56b38
PhakinGER <66070153@kmitl.ac.th>
2024-10-14 00:21:17 +0700

    AddImageSet

---
+++ src/main/java/com/Boardzone/boardapi/entity/ImageSet.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Boardzone.boardapi.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "imageset")
+public class ImageSet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int imageset_id;
+
+    @Lob
+    private byte[] image1;
+
+    @Lob
+    private byte[] image2;
+
+    @Lob
+    private byte[] image3;
+
+    @Lob
+    private byte[] image4;
+
+    @Lob
+    private byte[] image5;
+
+    public int getImageset_id() {
+        return imageset_id;
+    }
+
+    public void setImageset_id(int imageset_id) {
+        this.imageset_id = imageset_id;
+    }
+
+    public byte[] getImage1() {
+        return image1;
+    }
+
+    public void setImage1(byte[] image1) {
+        this.image1 = image1;
+    }
+
+    public byte[] getImage2() {
+        return image2;
+    }
+
+    public void setImage2(byte[] image2) {
+        this.image2 = image2;
+    }
+
+    public byte[] getImage3() {
+        return image3;
+    }
+
+    public void setImage3(byte[] image3) {
+        this.image3 = image3;
+    }
+
+    public byte[] getImage4() {
+        return image4;
+    }
+
+    public void setImage4(byte[] image4) {
+        this.image4 = image4;
+    }
+
+    public byte[] getImage5() {
+        return image5;
+    }
+
+    public void setImage5(byte[] image5) {
+        this.image5 = image5;
+    }
+}
